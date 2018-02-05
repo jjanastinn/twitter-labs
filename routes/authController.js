f846a1bc24ec90afbf0e0c377a66a35d5256a27c
Jana Stinn <jana-stinn@gmx.de>
2018-02-05 23:08:46 +0100

    login

--- routes/authController.js
+++ routes/authController.js
@@ -8,6 +8,10 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
+authController.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
 authController.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
@@ -45,11 +49,53 @@ authController.post('/signup', (req, res, next) => {
           errorMessage: 'Something went wrong when signing up'
         });
       } else {
-        req.session.currentUser = newUser;
-        res.redirect('/');
+        res.redirect('/login');
       }
     });
   });
+});
+
+authController.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+
+authController.post('/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Indicate a username and a password to log in'
+    });
+    return;
+  }
+
+  User.findOne({ 'username': username },
+    '_id username password following',
+    (err, user) => {
+      if (err || !user) {
+        res.render('auth/login', {
+          errorMessage: "The username doesn't exist"
+        });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          req.session.currentUser = user;
+          // logged in
+        } else {
+          res.render('auth/login', {
+            errorMessage: 'Incorrect password'
+          });
+        }
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        // res.redirect("/tweets");
+      } else {
+        res.render('auth/login', {
+          errorMessage: 'Incorrect password'
+        });
+      }
+    });
 });
 
 module.exports = authController;
